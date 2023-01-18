dfa21bbb513dc5448ffad7c25d7e0e43fca48dc9
faisal <ahmadfaisal47@gmail.com>
2023-01-18 12:27:56 +0430

    day of the programmer challenge done.

--- day-of-the-programmer.js
+++ day-of-the-programmer.js
@@ -1,11 +1,17 @@
 function dayOfProgrammer(year) {
     // Write your code here
-    var date = new Date();
-    var firstDay = new Date(date.getFullYear(), date.getMonth(), 1);
-    var lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0);
-
-    return [firstDay.getMonth(), lastDay]
+    const isLeapYear = (year) =>  {
+        if (year < 1917) {
+            return year % 4 === 0
+        } else {
+            return year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0)
+        }
+    }
+    if (year === 1918) {
+        return '26.09.1918'
+    }
+    return isLeapYear(year) ? `12.09.${year}` : `13.09.${year}`
 }
 
 console.clear();
-console.log(dayOfProgrammer(2016));
+console.log(dayOfProgrammer(1800));
